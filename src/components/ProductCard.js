04d4a46ac83d59681997f667e3f6ca8720380be5
Yixin88 <yixin88@hotmail.co.uk>
2023-01-24 22:05:41 +0000

    now product card fetch from api and update on screen

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,5 +1,4 @@
 export default function ProductCard(props) {
-
     function addToCart() {
         props.setCartItems(props.cartItems + 1)
     }
@@ -8,17 +7,22 @@ export default function ProductCard(props) {
             props.setCartItems(props.cartItems - 1)
         }
     }
-    
         return (
           <div className='product-card'>
-             <img className='product-img' src={props.imgUrl}/>
-             <h2 className='product-title'> {props.name}</h2> 
-             <p className='product-description'> {props.description}</p>
-    
+             <img className='product-img' src={props.image}/>
+                <p className='product-title'> {props.title}</p>
+             <div className="product-info">
+                <p className="price">{props.price}</p>
+                <div className="reviews">
+                    <p className="rate">rating: {props.rating.rate} </p>
+                    <p className="count">count: {props.rating.count} </p>
+                </div>
+             </div>
+             {/* <p className='product-description'> {props.description}</p> */}
+            
              <button onClick={() => addToCart()}>add to cart</button>
              <button onClick={() => deleteFromCart()}>remove from cart</button> 
            </div>
           )
        }
-       
        
