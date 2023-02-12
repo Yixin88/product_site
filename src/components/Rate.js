f3d287b72b6007cb5016212d1467c883408f3393
Yixin <yixin88@hotmail.co.uk>
2023-02-12 23:12:04 +0000

    added rate.js

---
+++ src/components/Rate.js
@@ -0,0 +1,11 @@
+// import React, { useState } from 'react'
+
+// export default function Rate() {
+//   const [ratingValue, setRatingValue] = useState(0)
+
+// //   const handleRating = (rate: number) => {
+// //     setRatingValue(rate)
+// //   }
+//   return (
+  
+//   ) }
