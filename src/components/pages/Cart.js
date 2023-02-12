0c341bf2c0a5cb68a1a274ac448f2e495a77f943
Yixin <yixin88@hotmail.co.uk>
2023-02-12 23:05:16 +0000

    fixed cart.js with new api

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -1,8 +1,29 @@
-export default function Cart ({cartItems}) {
+import React from "react"
+import { NavLink } from "react-router-dom"
+
+export default function Cart({cartItems, removeItem}) {
     return (
         <div>
-            <h2>shopping cart</h2>
-            <h4>total items : {cartItems}</h4>
+            <h2>Cart</h2>
+            <ul>
+                {(cartItems.length > 0) ? 
+
+                    cartItems.map((item,index) => 
+                        <li className="product-card" key={index} >
+                         <img className='product-img' src={item.thumbnail}/>
+                        <p className='product-title'> {item.title}</p>
+                        <p className="price">{item.price}</p>   
+                        <p>{item.title}</p>
+                        <button onClick={() => removeItem(item)}>remove from cart</button>
+                        </li>) 
+                    :
+                    <div>
+                        no items in cart. go
+                        <NavLink to={'/'}> here</NavLink> to add some
+                    </div>
+            }
+
+            </ul>
         </div>
     )
 }
