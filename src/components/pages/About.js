87051b6be4e11cb9a0af71b6cddea3ad6fe31c52
Yixin <yixin88@hotmail.co.uk>
2023-02-12 23:04:26 +0000

    added template code to about.js

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react' 
+
+export default function About(){
+  return (
+    <div>
+      <h2>no-brand shop is all about pasta</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quam, alias voluptatibus accusantium ab ducimus aut sunt et provident doloribus ullam, minus corrupti iste omnis cumque officiis quia vero. Perspiciatis.</p>
+    </div>
+  )
+}
