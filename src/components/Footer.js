cd616ee604e62c837bef0a465d96516c0c0470e5
Yixin <yixin88@hotmail.co.uk>
2023-02-12 23:01:14 +0000

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+    <footer>      
+        <div>
+            <ul>
+                <li><a href="https://twitter.com/">Twitter</a></li>
+                <li><a href="mailto:no-brand@gmail.com">Email</a></li>
+                <li><a href="https://github.com/no-brand">Github</a></li>
+
+                <li>
+                <p>👋</p>
+                </li>
+                <li>
+                <p className='logo'>no-brand</p>
+                </li>
+            </ul>
+        </div>
+    </footer> 
+    );
+}
+ 
+export default Footer;
